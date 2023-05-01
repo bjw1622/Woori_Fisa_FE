eca27292fa955f9b59349436a5abc50bbd70fec4
Jay <bjw1622@gmail.com>
2023-05-01 09:28:22 +0900

    Add : tailwind config

---
+++ 4.papago/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./public/index.html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        nanum: ["Nanum Gothic"],
+      },
+    },
+  },
+};
