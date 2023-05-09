1c2e51be0addad6f09469374748f1fc9f7425431
Jay <bjw1622@gmail.com>
2023-05-09 16:22:00 +0900

    feat : vue build

---
+++ 5.Vue/vue-todo/src/components/AppSection.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    {{ `TodoHeader 부분` }}<br />
+    {{ `TodoBody부분` }}
+  </div>
+</template>
+
+<script setup></script>
--- 5.Vue/vue-todo/src/views/HomeView.vue
+++ 5.Vue/vue-todo/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import AppHeader from "../components/AppHeader.vue";
+import AppSection from "../components/AppSection.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <AppHeader></AppHeader>
+    <AppSection></AppSection>
   </main>
 </template>
