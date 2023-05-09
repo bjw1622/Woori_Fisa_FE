47a04636bbe998a946c574c9e7ef0cf22375ea46
Jay <bjw1622@gmail.com>
2023-05-09 10:35:56 +0900

    feat : vue praticce

--- 5.Vue/vue-basic/src/6.list_rendering/Basic1ListRendering.vue
+++ 5.Vue/vue-basic/src/6.list_rendering/Basic1ListRendering.vue
@@ -1,17 +1,28 @@
 <template>
-	
+  <li v-for="(item, index) in itemArray" :key="item.id">
+    <p>{{ index }}</p>
+    <p>{{ item.message }}</p>
+    <p>{{ item.id }}</p>
+    <p>{{ parentMessage }}</p>
+  </li>
 </template>
 
 <script setup>
+import { ref } from "vue";
 
+const parentMessage = ref("Parent");
+const itemArray = [
+  { id: "i1", message: "Foo" },
+  { id: "i2", message: "Bar" },
+];
 </script>
 
 <style scoped>
 li {
-    margin: 1rem;
-    padding: 0.75rem;
-    border: 2px dashed blue;
-    list-style: none;
-    font-size: 1.5rem;
+  margin: 1rem;
+  padding: 0.75rem;
+  border: 2px dashed blue;
+  list-style: none;
+  font-size: 1.5rem;
 }
 </style>
