bd0ba4821a9cbb707a43486d2e1cb439490fd095
Jay <bjw1622@gmail.com>
2023-05-10 09:41:03 +0900

    feat : todoItem.vue

---
+++ 5.Vue/vue-todo/src/components/todos/TodoItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <li>
+      <div>
+        <span>{{ todo.category }}</span>
+        <div>
+          <h2>{{ todo.title }}</h2>
+          <p>{{ todo.summary }}</p>
+        </div>
+      </div>
+    </li>
+  </div>
+</template>
+
+<script setup></script>
