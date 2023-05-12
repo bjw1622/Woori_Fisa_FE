cee8b009cbf7bad8c2409cda1b776c4f5a273168
Jay <bjw1622@gmail.com>
2023-05-12 09:31:29 +0900

    feat : setting files

---
+++ 5.Vue/vitest-unit-basic/app.js
@@ -0,0 +1,21 @@
+const form = document.getElementById("form");
+const output = document.getElementById("result");
+
+const formSubmitHandler = (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+
+  const firstNumber = formData.get("firstNumber");
+  const secondNumber = formData.get("secondNumber");
+
+  const numbers = [];
+  numbers.push(firstNumber);
+  numbers.push(secondNumber);
+
+  // 덧셈 로직
+
+  output.textContent = result;
+};
+
+form.addEventListener("submit", formSubmitHandler);
