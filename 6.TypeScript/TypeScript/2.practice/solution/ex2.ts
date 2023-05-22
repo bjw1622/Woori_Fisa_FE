f1e310e8d73d7ccd89eccffaed61458974a511e8
Jay <bjw1622@gmail.com>
2023-05-22 12:24:45 +0900

    feat : ex2

--- 6.TypeScript/TypeScript/2.practice/solution/ex2.ts
+++ 6.TypeScript/TypeScript/2.practice/solution/ex2.ts
@@ -27,44 +27,44 @@ Exercise:
 */
 
 interface User {
-    name: string;
-    age: number;
-    occupation: string;
+  name: string;
+  age: number;
+  occupation: string;
 }
 
 interface Admin {
-    name: string;
-    age: number;
-    role: string;
+  name: string;
+  age: number;
+  role: string;
 }
 
 export type Person = User | Admin;
 
 export const persons: Person[] = [
-    {
-        name: 'Max Mustermann',
-        age: 25,
-        occupation: 'Chimney sweep'
-    },
-    {
-        name: 'Jane Doe',
-        age: 32,
-        role: 'Administrator'
-    },
-    {
-        name: 'Kate Müller',
-        age: 23,
-        occupation: 'Astronaut'
-    },
-    {
-        name: 'Bruce Willis',
-        age: 64,
-        role: 'World saver'
-    }
+  {
+    name: "Max Mustermann",
+    age: 25,
+    occupation: "Chimney sweep",
+  },
+  {
+    name: "Jane Doe",
+    age: 32,
+    role: "Administrator",
+  },
+  {
+    name: "Kate Müller",
+    age: 23,
+    occupation: "Astronaut",
+  },
+  {
+    name: "Bruce Willis",
+    age: 64,
+    role: "World saver",
+  },
 ];
 
 export function logPerson(person: Person) {
-    console.log(` - ${person.name}, ${person.age}`);
+  console.log(` - ${person.name}, ${person.age}`);
 }
 
 persons.forEach(logPerson);
