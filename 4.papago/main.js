dda5ae327a236c684cd3154668dc5eaf57e8da14
Jay <bjw1622@gmail.com>
2023-05-22 09:14:43 +0900

    feat : add typescript

--- 4.papago/main.js
+++ 4.papago/main.js
@@ -1,33 +1,18 @@
-const arr = [];
-const textInput = document.querySelector("#task-input");
-const addBtn = document.querySelector("#add-task");
-const todoList = document.querySelector(".todo-list");
-let inputText = "";
-textInput.addEventListener("change", (e) => {
-  inputText = e.target.value;
+// const num = document.querySelector("#num");
+// const add = document.querySelector("#add");
+// const answer = document.querySelector("#answer");
+// window.addEventListener("click", () => {
+//   console.log("test");
+// });
+// window.addEventListener("resize", (event) => {
+//   console.log(
+//     `높이:${event.target.innerHeight} 넓이:${event.target.innerWidth}`
+//   );
+// });
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+  console.log("121");
 });
-
-addBtn.addEventListener("click", () => {
-  const todo = {
-    id: Math.random().toString(16).slice(2),
-    inputText: inputText,
-  };
-  if (todo.inputText === "") {
-    return;
-  }
-  function test() {
-    console.log("test");
-  }
-  test();
-  arr.push(todo);
-  textInput.value = "";
-  inputText = "";
-  const todoDiv = `
-  <div class="todo">
-  <input type="checkbox" id="check-box" />
-  <div>${todo.inputText}</div>
-  <button id=del-button>삭제</button>
-  </div>
-  `;
-  todoList.innerHTML += todoDiv;
+window.addEventListener("click", () => {
+  console.log("test");
 });
---
+++ 6.TypeScript/TypeScript/1.basic_types/3.object/object.ts
@@ -0,0 +1,7 @@
+const dog = {
+    age: 2,
+    name: 'toto',
+    walk: function walk() {
+        console.log('강아지가 걷는다');
+    }
+};
